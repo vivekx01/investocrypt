48b18fb6e535413fd6ca3c5818303c785b8fa178
Vivek <72996825+vivekx01@users.noreply.github.com>
2023-01-29 13:35:30 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyC3FgY05tXji8KxjANlHo7ZgAWHQNHvXG4",
-    authDomain: "investo-crypt.firebaseapp.com",
-    projectId: "investo-crypt",
-    storageBucket: "investo-crypt.appspot.com",
-    messagingSenderId: "373020393212",
-    appId: "1:373020393212:web:f49c1b715b2c1666bc2179"
+    //api key here
   },
 };
 
